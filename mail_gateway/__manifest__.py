517078b0a94af11dc63da7f2bd756a81314b6c2a
Denis Vermylen <dve@odoo.com>
2017-08-09 10:10:21 +0200

    v6 + dbo
    Conflicts:
    	mail_gateway/__manifest__.py
    	mail_gateway/mail_gateway.xml

--- mail_gateway/__manifest__.py
+++ mail_gateway/__manifest__.py
@@ -3,8 +3,12 @@
 
 
 {
+<<<<<<< HEAD
 
     'version': "0.83",
+=======
+    'version': "0.6",
+>>>>>>> 9e8ff06... v6 + dbo
     'name': 'mail_gateway',
     'depends': ['mail'],
     'description': "dve testing",
