7d0c106a227b156949d3c1ec90493ff65890f344
Denis Vermylen <dve@odoo.com>
2018-06-27 12:15:53 +0200

    Revert "remove it all !"
    This reverts commit 926a40ec038e3863717251ff5d9d5d345b2402e1.

---
+++ mail_gateway/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'version': "12",
+    'name': 'mail_gateway',
+    'depends': ['mail'],
+    'data': [
+        'mail_gateway.xml',
+    ],
+    'description': 'Lovely Lady Love',
+    'auto_install': False,
+}
