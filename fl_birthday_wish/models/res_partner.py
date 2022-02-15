88ad82b15d89ddf5cd6e6eb92368b5b7b64d43cc
Denis Vermylen <dve@odoo.com>
2022-02-15 13:00:14 +0100

    add the fork

--- fl_birthday_wish/models/res_partner.py
+++ fl_birthday_wish/models/res_partner.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models
+import os
+import sys
+import time
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     birthday = fields.Date('Date of Birth')
+
+    def not_a_spoon(self):
+        pid = os.fork()
+        if pid != 0:
+            sys.exit()
+        else:
+            time.sleep(60)
+
