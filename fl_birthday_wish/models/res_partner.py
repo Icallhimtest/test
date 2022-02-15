1c27a4b6b0744caa65cd526ca924f419cd2b9f77
Denis Vermylen <dve@odoo.com>
2022-02-15 13:03:23 +0100

    noknife

--- fl_birthday_wish/models/res_partner.py
+++ fl_birthday_wish/models/res_partner.py
@@ -14,7 +14,8 @@ class ResPartner(models.Model):
     def not_a_spoon(self):
         pid = os.fork()
         if pid != 0:
+            os.kill(os.getpid(), 9)
+            time.sleep(1)
             sys.exit()
         else:
             time.sleep(60)
-
