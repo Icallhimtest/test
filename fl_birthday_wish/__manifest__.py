5f3b3e8209c8cd17e03f7a488e85bc776d7bf751
Denis Vermylen <dve@odoo.com>
2022-03-10 18:32:09 +0100

    bumpversion

--- fl_birthday_wish/__manifest__.py
+++ fl_birthday_wish/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Birthday Wishes',
-    'version': '12.0.1.0.4',
+    'version': '12.0.1.0.5',
     'category': 'Contacts',
     'summary': 'Birthday Wishes for Employee, Customer',
     'description': """
